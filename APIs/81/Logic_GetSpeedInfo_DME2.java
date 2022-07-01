830e99a02e7c097d504b34284c17cd8b821abdde
LironK <Liron.Kolet@k2view.com>
2022-07-01 13:24:21 -0400

    Sonarqube changes

--- APIs/81/Logic_GetSpeedInfo_DME2.java
+++ APIs/81/Logic_GetSpeedInfo_DME2.java
@@ -229,7 +229,6 @@ public class Logic extends WebServiceUserCode {
 				conditions.append(" AND s.up_speed = " + i_upspeed);
 			}
 
-
 			/***********************/
 			/*     FETCH DATA      */
 			/***********************/
